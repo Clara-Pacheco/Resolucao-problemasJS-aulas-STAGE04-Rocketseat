ff39c3071a76fcddeaed8b9596a2f8639978a631
Clara Pacheco <clarapachecodev@gmail.com>
2022-04-24 10:18:45 -0300

    Create folder aula02 with problem's information, solution and notes for aula02

---
+++ script.js
@@ -0,0 +1,8 @@
+/* 2-Solicite 2 números, faça a soma deles 
+  e apresente o resultado final so usuário */
+
+  alert('Iremos somar 2 números!')
+  let numberOne = prompt('Digite o primeiro número: ')
+  let numberTwo = prompt('Digite o segundo número: ')
+  let result = Number(numberOne) + Number(numberTwo)
+  alert('O resultado final é: ' + result)
