b4fb71577d7c38f95a7d084daf047b2563843218
Clara Pacheco <clarapachecodev@gmail.com>
2022-04-24 10:00:49 -0300

    Create folder aula01 and move files to it

---
+++ aula01/script.js
@@ -0,0 +1,12 @@
+/* 1-Encontre a solução ao problema:
+  Pergunte o nome do usuário e escreva a mensagem:
+
+  "Olá, [nome do usuário]"
+
+*/
+
+let nome = prompt('Qual é o seu nome?')
+alert('Olá, ' + nome)
+
+/* 2-Solicite 2 números, faça a soma deles 
+  e apresente o resultado final so usuário */
